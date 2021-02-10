5ef06a4101648da8c56c24b75d1a8014247d1837
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2021-02-10 03:02:55 +0200

    chore( style ): set main colors
        - add [ ./assets/main.css ] to ( CSS Global ) files in [ ./nuxt.config.js ]
        - add colors:
            - white
            - black
            - gray-100
            - gray-200
            - blue-100
            - blue-200

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,8 @@ export default {
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
     'normalize.css/normalize.css',
-    '@/assets/fonts/Cairo/load.css'
+    '@/assets/fonts/Cairo/load.css',
+    '@/assets/main.css'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
